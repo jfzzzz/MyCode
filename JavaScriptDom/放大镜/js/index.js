0a50845aeafe08c02847211c7891a00556febef4
jfzzzz <tiancai_zheng@163.com>
2018-08-12 09:49:18 +0800

    完善放大镜

--- JavaScriptDom/放大镜/js/index.js
+++ JavaScriptDom/放大镜/js/index.js
@@ -12,7 +12,6 @@ var bigBox = box.children[1];
 // 获取大图
 var bigImg = bigBox.children[0];
 
-
 // 1 鼠标移动到小盒子上 显示遮罩 和 显示大图
 smallBox.onmouseover = function () {
   mask.style.display = 'block';
@@ -22,6 +21,29 @@ smallBox.onmouseout = function () {
   mask.style.display = 'none';
   bigBox.style.display = 'none';
 }
-// 2 鼠标在小盒子里移动时，遮盖层随着鼠标移动
-
-// 3 显示对应的大图部分
+// 2 鼠标在小盒子里移动时，遮盖层随着鼠标移动。
+function onMouesemove(e) {
+  var e = e || window.event;
+  var x = e.clientX - box.offsetLeft - mask.offsetWidth / 2;
+  var y = e.clientY - box.offsetTop - mask.offsetHeight / 2;
+  console.log(smallBox.offsetWidth)
+  // 2.1 当遮罩层移动到小盒子的四周，定位在小盒子的四周  
+  if (x < 0) {
+    mask.style.left = 0 + 'px';
+  } else if (x + mask.offsetWidth > smallBox.offsetWidth) {
+    mask.style.left = smallBox.offsetWidth - mask.offsetWidth + 'px';
+  } else {
+    mask.style.left = x + 'px';
+  }
+  if (y < 0) {
+    mask.style.top = 0 + 'px';
+  } else if (y + mask.offsetHeight > smallBox.offsetHeight) {
+    mask.style.top = smallBox.offsetHeight - mask.offsetHeight + 'px';
+  } else {
+    mask.style.top = y + 'px';
+  }
+  // 3 显示对应的大图部分
+  bigImg.style.left = -(parseInt(mask.style.left) * 2) + 'px';
+  bigImg.style.top = -(parseInt(mask.style.top) * 2) + 'px';
+}
+smallBox.addEventListener('mousemove', onMouesemove, false);
